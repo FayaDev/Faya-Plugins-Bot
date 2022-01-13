461778a4406ce50826ecf958b5a0b48814c502e1
FayaDev <MSAZielhuis@gmail.com>
2022-01-13 19:45:04 +0100

    add ping latency

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,7 +1,12 @@
 module.exports = {
     name: 'ping',
     description: 'checks if the bot is online.',
-    execute(message, args){
-        message.reply("Pong!");
+    execute(message, client){
+        message.channel.send("Pinging...").then(m =>{
+            var ping = m.createdTimestamp - message.createdTimestamp;
+            //var botPing = Math.round(client.pi);
+
+            m.edit(`Pong! \`${ping}ms\``);
+        });        
     }
 }
