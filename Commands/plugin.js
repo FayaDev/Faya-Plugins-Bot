61470b8f5f97a6b17d956472c578095b1a5dfb14
FayaDev <MSAZielhuis@gmail.com>
2022-01-15 23:04:54 +0100

    improve plugin.js

--- Commands/plugin.js
+++ Commands/plugin.js
@@ -1,30 +1,29 @@
 const { Permissions, MessageEmbed } = require("discord.js");
 
 module.exports = {
-    name: 'release',
-    description: 'announce a new release.',
+    name: 'plugin',
+    description: '',
     execute(message, args){
         if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES) || message.author.bot) return message.channel.send(`${message.member}, you are not allowed to perform this action.`);
         
         const member = message.mentions.members.first();
-        let plugin = args[0];
+        let plugin = args[0].replaceAll('_', ' ');
         let url = args[1];
         let image = args[2];
-        let announcement = args.slice(3).join(" ");
+        let details = args.slice(3).join(" ");
 
         if (!plugin) return message.reply("You must enter a plugin name!");
         if (!url) return message.reply("You must enter a URL!");
         if (!image) return message.reply("You must enter an image link!");
-        if (!announcement) return message.reply("You must enter an announcement!");
+        if (!details) return message.reply("You must enter an announcement!");
 
-        const announceEmbed = new MessageEmbed()
-            .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL() })
-            .setTitle(plugin.replace('_', ' '))
+        const pluginEmbed = new MessageEmbed()
+            .setTitle(plugin)
             .setURL(url)
-            .setDescription(`**${announcement}**`)
+            .setDescription(`**${details}**`)
             .setImage(image)
 
         message.delete();
-        message.channel.send({ embeds: [announceEmbed] });
+        message.channel.send({ embeds: [pluginEmbed] });
         }
 }
