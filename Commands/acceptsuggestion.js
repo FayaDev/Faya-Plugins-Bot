791af975db0970bf864b1e08e50119071b1f1a0a
FayaDev <MSAZielhuis@gmail.com>
2022-01-18 20:40:46 +0100

    Add acceptsuggestion

--- Commands/acceptsuggestion.js
+++ Commands/acceptsuggestion.js
@@ -6,16 +6,24 @@ module.exports = {
         if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES) || message.author.bot) return message.channel.send(`${message.member}, you are not allowed to perform this action.`);
     
         const messageId = args[0];
-        const suggestionChannel = message.guild.channels.cache.get('932364904193740910');
+        const suggestionChannel = message.guild.channels.cache.get('930206093676781569');
         const suggestedEmbed = await suggestionChannel.messages.fetch(messageId);
         const data = suggestedEmbed.embeds[0];
 
+        if (!messageId) return message.reply("You need to enter the ID of the suggestion.");
+        if (!data) return message.reply("No embed has been found.");
+
         const acceptEmbed = new MessageEmbed()
             .setAuthor({ name: data.author.name, iconURL: data.author.iconURL})
-            .setTitle("Suggestion Accepted")
+            .setTitle("Suggestion")
             .setDescription(data.description)
+            .addField("__Status:__", "✅ Accepted")
             .setColor("DARK_GREEN")
 
-        message.channel.send({ embeds: acceptEmbed });
+
+        //const user = await client.users.cache.find((u) => u.tag === data.author.name);
+
+        //user.send("Your suggestion has been accepted by a moderator!");
+        message.channel.send({ embeds: [acceptEmbed] });
     }
 }
