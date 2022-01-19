82d79d2936fb641141ad17f6db3cebdf2df262a0
FayaDev <msazielhuis@gmail.com>
2022-01-19 10:37:24 +0100

    Update help command

--- Commands/help.js
+++ Commands/help.js
@@ -9,11 +9,12 @@ module.exports = {
             .setTitle("Help")
             .setDescription("**A list of all the commands available**")   
             .addField("1) !ping", "> !ping")
-            .addField("2) !suggest", "> !suggest [message ID]")
-            .addField("3) !issue", "> !issue [issue]")
-            .addField("4) !kick", "> !kick [member]")
-            .addField("5) !ban", "> !ban [member] (reason)")
-            .addField("6) !plugin", "> !plugin [name] [url] [image url] [description]")
+            .addField("2) !clear", "> !clear [number]")
+            .addField("3) !suggest", "> !suggest [message ID]")
+            .addField("4) !issue", "> !issue [issue]")
+            .addField("5) !kick", "> !kick [member]")
+            .addField("6) !ban", "> !ban [member] (reason)")
+            .addField("7) !plugin", "> !plugin [name] [url] [image url] [description]")
 
         message.channel.send({ embeds: [helpEmbed] });
     }
