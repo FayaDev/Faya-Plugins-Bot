d2dac8d6a78a035759ef3a93c2aef6d19b71ea14
FayaDev <MSAZielhuis@gmail.com>
2022-01-20 10:35:51 +0100

    Improve logging system

--- Events/member-count.js
+++ Events/member-count.js
@@ -1,14 +1,15 @@
 module.exports = client => {
 
-    const updateMembers = (guild, log) => {
+    const updateMembers = (guild) => {
         const channel = guild.channels.cache.get('931958980064997376');
-        channel.setName(`Members: ${guild.members.cache.filter(m => !m.user.bot).size.toLocaleString()}`)
-        if (log) { console.log(`[Yubu]: Updated member count to ${humans}`); }
+        let humans = guild.members.cache.filter(m => !m.user.bot).size.toLocaleString();
+        channel.setName(`Members: ${humans}`)
+        console.log(`[Yubu]: Detected ${humans} members`);
         
     }
 
-    client.on('guildMemberAdd', (member) => updateMembers(member.guild, true));
-    client.on('guildMemberRemove', (member) => updateMembers(member.guild, true));
+    client.on('guildMemberAdd', (member) => updateMembers(member.guild));
+    client.on('guildMemberRemove', (member) => updateMembers(member.guild));
     
-    updateMembers(client.guilds.cache.get('844917410904670248'), false);
+    updateMembers(client.guilds.cache.get('844917410904670248'));
 }
--- main.js
+++ main.js
@@ -8,20 +8,25 @@ const intents = new Discord.Intents(32767);
 
 const client = new Discord.Client({ intents });
 client.on('ready', () => { 
-    console.log("[Yubu]: Yubu is online!"); 
-    
     memberCount(client)
+
+    console.log("[Yubu]: Yubu is online!"); 
 })
 
 const memberCount = require('./Events/member-count')
+let fileCount = 0;
 
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for (const file of commandFiles){
-    const command = require(`./Commands/${file}`);
 
+for (const file of commandFiles){
+    fileCount++;
+
+    const command = require(`./Commands/${file}`);
     client.commands.set(command.name, command);
 }
+
+console.log(`[Yubu]: Loaded ${fileCount} commands`);
 
 client.on('messageCreate', message => {
     if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
@@ -29,32 +34,12 @@ client.on('messageCreate', message => {
     const args = message.content.slice(process.env.PREFIX.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if (command === 'ping'){
-        client.commands.get('ping').execute(message, args);
-    } else if (command == 'help'){
-        client.commands.get('help').execute(message, args);
-    } else if (command == 'kick'){
-        client.commands.get('kick').execute(message, args);
-    } else if (command == 'ban'){
-        client.commands.get('ban').execute(message, args);
-    }else if (command == 'issue'){
-        client.commands.get('issue').execute(message, args);
-    }else if (command == 'reissue'){
-        client.commands.get('reissue').execute(message, args);
-    }else if (command == 'plugin'){
-        client.commands.get('plugin').execute(message, args);
-    }else if (command == 'say'){
-        client.commands.get('say').execute(message, args);
-    }else if (command == 'sayembed'){
-        client.commands.get('sayembed').execute(message, args);
-    }else if (command == 'suggest'){
-        client.commands.get('suggest').execute(message, args);
-    }else if (command == 'acceptsuggestion'){
-        client.commands.get('acceptsuggestion').execute(message, args);
-    }else if (command == 'denysuggestion'){
-        client.commands.get('denysuggestion').execute(message, args);
-    }else if (command == 'clear'){
-        client.commands.get('clear').execute(message, args);
+    for (const file of commandFiles){
+        const fileNoExtension = file.slice(0, -3);
+
+        if (command === fileNoExtension){
+            client.commands.get(fileNoExtension).execute(message, args);
+        }
     }
 
     console.log(`[Yubu]: ${message.author.tag} used ${message.content}`);
