c8f52da3d955f4dac1fe07b7beff2ea48bf9582d
FayaDev <MSAZielhuis@gmail.com>
2022-01-31 00:43:35 +0100

    Add lockdown command

--- Commands/clear.js
+++ Commands/clear.js
@@ -8,14 +8,16 @@ module.exports = {
         try {
             if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return message.reply("You don't have the required permissions to perform this action.");
         
-            if (!args[0]) return message.reply("You must enter an amount to clear.");
-            if (isNaN(args[0])) return message.reply("You must enter a number.");
+            let amount = args[0];
 
-            if (args[0] > 100) return message.reply("You can't delete more than 100 messages.");
-            if (args[0] < 1) return message.reply("You must delete atleast 1 message.");
+            if (!amount) return message.reply("You must enter an amount to clear.");
+            if (isNaN(amount)) return message.reply("You must enter a number.");
 
-            message.channel.bulkDelete(args[0] + 1).then(messages => {
-                message.channel.send(`Deleted **${messages.size - 1}** messages.`).then(msg => {setTimeout(() => msg.delete(), 3000)});
+            if (amount > 100) return message.reply("You can't delete more than 100 messages.");
+            if (amount < 1) return message.reply("You must delete at least 1 message.");
+
+            message.channel.bulkDelete(amount).then(messages => {
+                message.channel.send(`Deleted **${messages.size}** messages.`).then(msg => {setTimeout(() => msg.delete(), 3000)});
             });
         } catch (err) {
            console.log(err); 
--- Commands/lockdown.js
+++ Commands/lockdown.js
@@ -3,9 +3,28 @@ const { Permissions } = require('discord.js');
 module.exports = {
     name: 'lockdown',
     usage: '',
-    execute(client, message){
+    async execute(client, message, args){
         if (!message.member.permissions.has('MANAGE_MESSAGES')) return message.reply("You don't have the required permissions to perform this action.");
-        
-        
+
+        let role = message.guild.roles.cache.find(r => r.id === "934131193564262500");
+
+        if (!args.length) return message.reply("Please enter a query.");
+
+        const query = args[0].toLowerCase();
+
+        if(!['true', 'false'].includes(query)) return message.reply("The option is not valid.");
+
+        const perms = role.permissions.toArray();
+
+        if (query == 'false') {
+            perms.push('SEND_MESSAGES');
+            await role.edit({ permissions: perms });
+            message.reply("Server is no longer locked down.");   
+        } else if (query == 'true') {
+            const newPerms = perms.filter((perm) => perm !== 'SEND_MESSAGES');
+
+            await role.edit({ permissions: newPerms });
+            message.reply("Server is locked down.");
+        }
     }
 }
