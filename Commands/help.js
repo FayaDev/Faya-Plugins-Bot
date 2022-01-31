3a0ff1033dd5eb83143805eda8a3a2f901e20be2
FayaDev <MSAZielhuis@gmail.com>
2022-01-31 16:46:24 +0100

    Improve help command

--- Commands/help.js
+++ Commands/help.js
@@ -1,28 +1,33 @@
 const { MessageEmbed } = require("discord.js");
-const fs = require('fs');
 
 module.exports = {
     name: "help",
     description: "sends a message with all the commands",
     usage: '',
-    execute(client, message){
+    execute(client, message, args){
         try {
-            const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-            let commandNumber = 0;
 
+            const typeHelp = args[0];
             const helpEmbed = new MessageEmbed()
-                .setTitle("Help")
-                .setDescription("**A list of all the commands available**")
+            helpEmbed.setTitle("Help")
 
-            for (let file in commandFiles){
-                file = commandFiles[commandNumber].replace(".js", "");
-                fileData = require(`./${commandFiles[commandNumber]}`);
-                helpEmbed.addField(`${commandNumber + 1}) !${file}`, `> !${file} ${fileData.usage}`);
-                commandNumber++;
+            if (typeHelp && typeHelp == "mod") {
+                helpEmbed.setDescription("**A list of all the __moderation__ commands.**")
+                helpEmbed.addField("!warn [user] (reason)", "> Warns a user.")
+                helpEmbed.addField("!kick [user] (reason)", "> Kicks a user.")
+                helpEmbed.addField("!ban [user] [duration] (reason)", "> Bans a user.")
+                helpEmbed.addField("!clear [number]", "> Clears a set amount of messages.")
+                helpEmbed.addField("!lockdown [true/false]", "> Stops users from sending messages in every channel.")
+                helpEmbed.addField("!say [text]", "> Says a specified message.")
+                helpEmbed.addField("!poll [\"option1\"] [\"option2\"]", "> Sends a poll with 2 options in the poll channel.")
+            } else {
+                helpEmbed.setDescription("**A list of all the __member__ commands.**")
+                helpEmbed.addField("!ping", "> Shows if the bot is online with the latency.")
+                helpEmbed.addField("!suggest", "> Creates a suggestion in the suggestion channel.")
             }
+
             message.delete();
-            message.channel.send({ embeds: [helpEmbed] });
-            message.channel.send("```\n[ ] = This parameter is required\n{ } = Words must be separated with an underscore\n( ) = This parameter is optional\n```")
+            message.channel.send({ embeds: [helpEmbed] })
         } catch (error) {
             console.log(error);
         }
