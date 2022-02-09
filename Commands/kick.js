9d36b547ba1d101e3cd4ccf7b0a7aef3405fcb4e
FayaDev <MSAZielhuis@gmail.com>
2022-02-09 22:01:03 +0100

    Fix adding reason

--- Commands/kick.js
+++ Commands/kick.js
@@ -7,7 +7,7 @@ module.exports = {
             if (!message.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)) return message.reply("You don't have the required permissions to perform this action.");
         
             const member = message.mentions.members.first();
-            let reason = args[1];
+            let reason = args.slice(1).join(" ");
             if (!member) return message.reply("You must enter a user.");
             if (!reason) reason = "an unknown reason";
 
