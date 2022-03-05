f8c014a0140dc08128a87b11b841fc3984e66f1c
FayaDev <MSAZielhuis@gmail.com>
2022-03-05 22:52:52 +0100

    Enhance getting delay channel

--- Commands/delay.js
+++ Commands/delay.js
@@ -9,6 +9,7 @@ module.exports = {
             const project = args[0].replaceAll('_', ' ');
             const date = args[1].replaceAll('_', ' ');
             const reason = args.slice(2).join(" ");
+            const delayChannel = message.guild.channels.cache.find(delChannel => delChannel.name.includes('delay'));
 
             if (!project) return message.reply("You need to enter a project.");
             if (!date) return message.reply("You need to enter a delay date.");
@@ -19,7 +20,6 @@ module.exports = {
 
             if (reason) {delayEmbed.addField("Reason:", reason)}
 
-            const delayChannel = message.guild.channels.cache.get('934211572375625729');
             delayChannel.send({ embeds: [delayEmbed] });
         } catch (error) {
             console.log(error);
