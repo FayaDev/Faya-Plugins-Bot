0aa6d5efc5ca1b9816c6a0b9f82ca4196031e468
FayaDev <MSAZielhuis@gmail.com>
2022-03-06 12:09:01 +0100

    Improve meme command

--- Commands/meme.js
+++ Commands/meme.js
@@ -6,21 +6,25 @@ module.exports = {
     name: "meme",
     async execute(client, message, args, Discord){
         try {
-            let reddit = [
-                "meme",
-                "dankmemes",
-            ]
+            let reddit = ["dankmemes", "memes"]
         
-            let subreddit = reddit[Math.floor(Math.random() * reddit.length)];
-        
-            randomPuppy(subreddit).then(async url => {
-                    await message.channel.send({
-                        files: [{
-                            attachment: url,
-                            name: 'meme.png'
-                        }]
-                    });
-            }).catch(err => console.error(err));
+            let subreddit = reddit[Math.floor(Math.random() * reddit.length + 1)];
+            const img = await randomPuppy(subreddit);
+            
+            if (subreddit == "undefined") return;
+
+            const memeEmbed = new MessageEmbed()
+                .setTitle(`r/${subreddit}`)
+                .setURL(`https://reddit.com/r/${subreddit}`)
+                .setColor("RANDOM")
+                .setImage(img)
+
+
+            message.channel.send({ embeds: [memeEmbed] });
+            if (memeEmbed.title == null) {
+                message.delete();
+                message.channel.send({ embeds: [memeEmbed] });
+            }
 
         } catch (error) {
             console.log(error);
