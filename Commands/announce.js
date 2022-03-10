0b3b852a2e30013d370fa3036e67e42134369ffa
FayaDev <MSAZielhuis@gmail.com>
2022-03-10 11:51:34 +0100

    Update announce command

--- Commands/announce.js
+++ Commands/announce.js
@@ -5,26 +5,34 @@ module.exports = {
     execute(client, message, args){
         try {
             if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return message.reply("You don't have the required permissions to perform this action.");
-        
-            let plugin = args[0].replaceAll('_', ' ');
-            let url = args[1];
-            let image = args[2];
-            let details = args.slice(3).join(" ");
 
-            if (!plugin) return message.reply("You must enter a plugin name.");
-            if (!url) return message.reply("You must enter a URL.");
-            if (!image) return message.reply("You must enter an image link.");
-            if (!details) return message.reply("You must enter an announcement.");
+            const msg = args.slice(0).join(" ");
+            const title = msg.match(/(?:"[^"]*"|^[^"]*$)/)[0].replace(/"/g, "")
+            const announcement = msg.replace(title, "").replace(/"/g, "")
+            let regex = /http[%\?#\[\]@!\$&'\(\)\*\+,;=:~_\.-:\/ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789]*/;
 
-            const pluginEmbed = new MessageEmbed()
-                .setTitle(plugin)
-                .setURL(url)
-                .setDescription(`**${details}**`)
-                .setImage(image)
+            const announceEmbed = new MessageEmbed()
+                .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL() })
+                .setTitle(title)
+                .setDescription(announcement)
+
+            // args.forEach(element => {
+            //     if (element.startsWith("https://")) {
+            //         announceEmbed.setURL(element)
+            //         announceEmbed.setDescription(announcement.replace(regex, ""))                    
+
+            //     }
+
+            //     if (element.startsWith("https://i")) {
+            //         announceEmbed.setImage(element);
+            //         announceEmbed.setDescription(announcement.replace(regex, ""))                    
+            //     } 
+            // });
 
             message.delete();
-            message.channel.send("<@&928005166580531230>");
-            message.channel.send({ embeds: [pluginEmbed] });
+
+            message.channel.send({ embeds: [announceEmbed] });
+
         } catch (error) {
             console.log(error);
         }
