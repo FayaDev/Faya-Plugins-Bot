fc6e71ff04e04f6062b5cf065bbe85a9c9538faa
FayaDev <MSAZielhuis@gmail.com>
2022-03-10 12:22:21 +0100

    Finish announce command

--- Commands/announce.js
+++ Commands/announce.js
@@ -1,4 +1,4 @@
-const { Permissions, MessageEmbed } = require("discord.js");
+const { Permissions, MessageEmbed, LimitedCollection } = require("discord.js");
 
 module.exports = {
     name: 'announce',
@@ -7,30 +7,23 @@ module.exports = {
             if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return message.reply("You don't have the required permissions to perform this action.");
 
             const msg = args.slice(0).join(" ");
-            const title = msg.match(/(?:"[^"]*"|^[^"]*$)/)[0].replace(/"/g, "")
-            const announcement = msg.replace(title, "").replace(/"/g, "")
-            let regex = /http[%\?#\[\]@!\$&'\(\)\*\+,;=:~_\.-:\/ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789]*/;
+            const title = msg.match(/(?:"[^"]*"|^[^"]*$)/)[0].replace(/"/g, "");
+            let announcement = msg.replace(title, "").replace(/"/g, "");
+            
+            let imgURL = announcement.split(" ").splice(-1)[0];;
+            let pageURL = announcement.split(" ").splice(-2)[0];;
+            
+            announcement = announcement.replace(imgURL, "")
+            announcement = announcement.replace(pageURL, "")
 
             const announceEmbed = new MessageEmbed()
                 .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL() })
                 .setTitle(title)
                 .setDescription(announcement)
-
-            // args.forEach(element => {
-            //     if (element.startsWith("https://")) {
-            //         announceEmbed.setURL(element)
-            //         announceEmbed.setDescription(announcement.replace(regex, ""))                    
-
-            //     }
-
-            //     if (element.startsWith("https://i")) {
-            //         announceEmbed.setImage(element);
-            //         announceEmbed.setDescription(announcement.replace(regex, ""))                    
-            //     } 
-            // });
+                .setImage(imgURL)
+                .setURL(pageURL)
 
             message.delete();
-
             message.channel.send({ embeds: [announceEmbed] });
 
         } catch (error) {
