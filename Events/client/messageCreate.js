bc0711f6e430c5a5d40f50f0ddf013bf7276a243
FayaDev <MSAZielhuis@gmail.com>
2022-03-10 11:49:42 +0100

    Remove automatic thread making

--- Events/client/messageCreate.js
+++ Events/client/messageCreate.js
@@ -9,19 +9,15 @@ module.exports = async (Discord, client, message, args) => {
 
         const suggestEmbed = new MessageEmbed()
             .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL() })
-            .setTitle("Suggestion")
+            .setTitle(`Suggestion`)
             .setDescription(message.toString())
             .setColor("#337fd5")
             .addField("__Status:__ 📊", "Waiting for community feedback.")
-            .setFooter({ text: "Want to suggest something? Simply type it in this channel!" })
-                
+            .setFooter({ text: "Want to suggest something? Simply type it in this channel!" })    
+
         await suggestionChannel.send({ embeds: [suggestEmbed] }).then(embedMessage => {
             embedMessage.react('⬆️'); 
             embedMessage.react('⬇️'); 
-            embedMessage.startThread({
-                name: "Discuss this suggestion",
-                autoArchiveDuration: 1440,
-            })
         });
 
         message.delete();
