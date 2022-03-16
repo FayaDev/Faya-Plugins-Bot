0ed2e65e4f7a674ae77d7eeb0277fc00b45780a9
FayaDev <msazielhuis@gmail.com>
2022-03-16 23:36:40 +0100

    Add welcome message

--- main.js
+++ main.js
@@ -11,4 +11,18 @@ client.events = new Discord.Collection();
     require(`./Handlers/${handler}`)(client, Discord);
 })
 
+const welcomeChannelId = '953772512502747187';
+
+client.on('guildMemberAdd', member => {
+    const welcomeMessage = new Discord.MessageEmbed()
+        .setAuthor({ name: member.user.tag, iconURL: member.displayAvatarURL() })
+        .setDescription(`Welcome **${member.user.username}**, enjoy your stay!`)
+        .setThumbnail(member.displayAvatarURL())
+        .setColor("RANDOM")
+        .setFooter({ text: member.guild.name, iconURL: member.guild.iconURL()})
+
+    const channel  = member.guild.channels.cache.get(welcomeChannelId);
+    channel.send({ embeds: [welcomeMessage] });
+})
+
 client.login(process.env.TOKEN);
