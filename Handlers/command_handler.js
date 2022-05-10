e4d39425ee4d1737ee29cd0a18251dbae34518b7
Milan Zielhuis <MSAZielhuis@gmail.com>
2022-05-10 10:14:24 +0200

    Remove moderation commands

--- Handlers/command_handler.js
+++ Handlers/command_handler.js
@@ -1,20 +1,9 @@
 const fs = require('fs');
 
 module.exports = (client, Discord) => {
-    const command_files_moderation = fs.readdirSync('./Commands/Moderation/').filter(file => file.endsWith('.js'));
     const command_files_suggestion = fs.readdirSync('./Commands/Suggestion/').filter(file => file.endsWith('.js'));
     const command_files_general = fs.readdirSync('./Commands/General/').filter(file => file.endsWith('.js'));
     const command_files_other = fs.readdirSync('./Commands/Other/').filter(file => file.endsWith('.js'));
-
-    for (const file of command_files_moderation) {
-        const command = require(`../Commands/Moderation/${file}`);
-
-        if (command.name) {
-            client.commands.set(command.name, command);
-        } else {
-            continue;
-        }
-    }
 
     for (const file of command_files_suggestion) {
         const command = require(`../Commands/Suggestion/${file}`);
