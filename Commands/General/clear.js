0a2e74fb3618f083cb27732c65c0e0860d29cc63
Milan Zielhuis <MSAZielhuis@gmail.com>
2022-06-21 10:34:39 +0200

    Update clear command

--- Commands/General/clear.js
+++ Commands/General/clear.js
@@ -1,21 +1,35 @@
-const { Permissions } = require('discord.js');
+const { Permissions, MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'clear',
-    async execute(client, message, args){
+    async execute(client, message, args, interaction){
         try {
             if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return message.reply("You don't have the required permissions to perform this action.");
         
-            let amount = args[0];
+            const logChannel = message.guild.channels.cache.get('987472077760122950');
+
+            const amount = args[0];
+            const reason = args.slice(1).join(" ");
 
             if (!amount) return message.reply("You must enter an amount to clear.");
+            if (!reason) return message.reply("You must enter a reason.");
             if (isNaN(amount)) return message.reply("You must enter a number.");
 
             if (amount > 100) return message.reply("You can't delete more than 100 messages.");
-            if (amount < 1) return message.reply("You must delete at least 1 message.");
+            if (amount < 2) return message.reply("You must delete at least 2 messages.");
+
+            const logEmbed = new MessageEmbed()
+                .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL() })
+                .setColor("ORANGE")
+                .setFooter({ text: `User ID: ${message.author.id}` })
 
             message.channel.bulkDelete(amount).then(messages => {
-                message.channel.send(`Deleted **${messages.size}** messages.`);
+                message.channel.send(`Deleted **${messages.size}** messages.`).then(msg => {setTimeout(() => msg.delete(), 3000)});
+                message.channel.send(`\n${message.author}, fill out an action report.`).then(msg => {setTimeout(() => msg.delete(), 7000)});
+
+                logEmbed.setDescription(`**Deleted Messages:** ${messages.size}\n**Channel:** ${message.channel}\n**Reason:** ${reason}`)
+
+                logChannel.send({ embeds: [logEmbed] })
             });
         } catch (err) {
            console.log(err); 
--- Commands/Suggestion/denysuggestion.js
+++ Commands/Suggestion/denysuggestion.js
@@ -7,19 +7,19 @@ module.exports = {
             if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return message.reply("You don't have the required permissions to perform this action.");
     
             const messageId = args[0];
-            let note = args.slice(1).join(" ")
+            let note = args.slice(1).join(" ");
 
-            let suggestionChannel = message.guild.channels.cache.get('930206093676781569')
+            let suggestionChannel = message.guild.channels.cache.get('930206093676781569');
             const suggestedEmbed = await suggestionChannel.messages.fetch(messageId);
             const data = suggestedEmbed.embeds[0];
 
             if (!messageId) return message.reply("You need to enter the ID of the suggestion.");
             if (!data) return message.reply("No embed has been found.");
             if (!suggestedEmbed) return message.reply("No embed has been found.");
-            if (!note) note = "Denied."
+            if (!note) note = "Denied.";
 
             const denyEmbed = new MessageEmbed()
-                .setAuthor({ name: data.author.name, iconURL: data.author.iconURL})
+                .setAuthor({ name: data.author.name, iconURL: data.author.iconURL })
                 .setTitle("Suggestion")
                 .setDescription(data.description)
                 .addField("__Status:__ ⛔", `> ${note}`)
